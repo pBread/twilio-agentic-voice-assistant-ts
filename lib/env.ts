e98204af4ee05c78c329483d5531e11339dfdd10
pBread <4473570+pBread@users.noreply.github.com>
2025-02-07 10:34:15 -0700

    updating regex

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,5 @@
 import "dotenv-flow/config"; // supports multiple environments, e.g. .env, .env.local
+import log from "./logger";
 
 export const HOSTNAME = process.env.HOSTNAME as string;
 export const PORT = process.env.PORT as string;
@@ -6,10 +7,10 @@ export const PORT = process.env.PORT as string;
 /****************************************************
  Validation Checks
 ****************************************************/
-if (!HOSTNAME) console.error(`Missing env var HOSTNAME`);
-const hostReg =
-  /^[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?)*$/;
-if (!hostReg.test(HOSTNAME))
-  console.error(
+if (!HOSTNAME) log.red(`Missing env var HOSTNAME`);
+const hostnameRFC1123 =
+  /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
+if (!hostnameRFC1123.test(HOSTNAME))
+  log.red(
     `Invalid HOSTNAME. Only include the the hostname, e.g. domain.com or sub.domain.com, not the other URL elements, e.g. http://`
   );
