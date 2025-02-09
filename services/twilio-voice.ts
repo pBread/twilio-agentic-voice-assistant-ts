9a2db645bbfefad28fe1f191095357025c11b1c7
pBread <4473570+pBread@users.noreply.github.com>
2025-02-09 13:08:53 -0700

    removed useless comments

--- services/twilio-voice.ts
+++ services/twilio-voice.ts
@@ -1,12 +1,12 @@
 export async function startRecording(callSid: string) {}
 
 export interface TwilioCallWebhookPayload {
-  CallSid: string; // The unique ID of the call
-  AccountSid: string; // The Account SID that initiated the call
-  From: string; // The phone number or Client identifier of the party calling
-  To: string; // The phone number or Client identifier of the called party
-  ApiVersion: string; // API version used to make the call
-  Direction: "inbound" | "outbound-api"; // The direction of the call (inbound or outbound-api)
+  CallSid: string;
+  AccountSid: string;
+  From: string;
+  To: string;
+  ApiVersion: string;
+  Direction: "inbound" | "outbound-api";
   CallStatus:
     | "queued"
     | "ringing"
@@ -14,22 +14,22 @@ export interface TwilioCallWebhookPayload {
     | "completed"
     | "busy"
     | "failed"
-    | "no-answer"; // The status of the call
+    | "no-answer";
 
-  CallCost?: string; // The price charged for the call
-  CallerId?: string; // A custom string that was passed when initiating the call
-  CallerName?: string; // The caller name if the call was initiated with CNAM lookup enabled
-  Duration?: string; // The length of the call in seconds
-  ForwardedFrom?: string; // The forwarded phone number if the call was forwarded
-  FromCity?: string; // The city of the caller
-  FromCountry?: string; // The country of the caller
-  FromState?: string; // The state or province of the caller
-  FromZip?: string; // The zip code of the caller
-  RecordingSid?: string; // The unique ID of the recording
-  RecordingUrl?: string; // The URL of the recording
-  StartTime?: string; // The time the call started
-  ToCity?: string; // The city of the called party
-  ToCountry?: string; // The country of the called party
-  ToState?: string; // The state or province of the called party
-  ToZip?: string; // The zip code of the called party
+  CallCost?: string;
+  CallerId?: string;
+  CallerName?: string;
+  Duration?: string;
+  ForwardedFrom?: string;
+  FromCity?: string;
+  FromCountry?: string;
+  FromState?: string;
+  FromZip?: string;
+  RecordingSid?: string;
+  RecordingUrl?: string;
+  StartTime?: string;
+  ToCity?: string;
+  ToCountry?: string;
+  ToState?: string;
+  ToZip?: string;
 }
