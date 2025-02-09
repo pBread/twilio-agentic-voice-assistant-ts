57f9f5b3c574d1600dc72c955fc5c265b7dfb465
pBread <4473570+pBread@users.noreply.github.com>
2025-02-09 13:09:57 -0700

    env vars

--- lib/env.ts
+++ lib/env.ts
@@ -1,16 +1,29 @@
 import "dotenv-flow/config"; // supports multiple environments, e.g. .env, .env.local
 import log from "./logger";
 
-export const HOSTNAME = process.env.HOSTNAME as string;
 export const PORT = process.env.PORT as string;
 
 /****************************************************
- Validation Checks
+ Required
 ****************************************************/
+export const HOSTNAME = process.env.HOSTNAME as string;
+export const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID as string;
+
+export const TWILIO_API_KEY = process.env.TWILIO_API_KEY as string;
+export const TWILIO_API_SECRET = process.env.TWILIO_API_SECRET as string;
+
+if (!TWILIO_ACCOUNT_SID) log.red("Missing env var TWILIO_ACCOUNT_SID");
+if (!TWILIO_API_KEY) log.red("Missing env var TWILIO_API_KEY");
+if (!TWILIO_API_SECRET) log.red("Missing env var TWILIO_API_SECRET");
+
+// validate the hostname is included then
 if (!HOSTNAME) log.red(`Missing env var HOSTNAME`);
-const hostnameRFC1123 =
-  /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
-if (!hostnameRFC1123.test(HOSTNAME))
-  log.red(
+else if (
+  // RFC-1123
+  !/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(
+    HOSTNAME
+  )
+)
+  log.warn(
     `Invalid HOSTNAME. Only include the the hostname, e.g. domain.com or sub.domain.com, not the other URL elements, e.g. http://`
   );
