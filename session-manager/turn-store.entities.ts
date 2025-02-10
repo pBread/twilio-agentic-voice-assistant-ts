03c129326fa592db1eb8963a52fc8552b757e375
pBread <4473570+pBread@users.noreply.github.com>
2025-02-10 09:44:24 -0700

    ported add params

--- session-manager/turn-store.entities.ts
+++ session-manager/turn-store.entities.ts
@@ -13,9 +13,12 @@ interface StoreTurn {
   order: number; // order is a non-sequential incrementor. Each turn is only gauranteed to have an order value greater than the previous. It's not always +1 greater than the previous.
 }
 
-// bot turn entities
+/****************************************************
+ Bot Turns
+****************************************************/
 export type BotTurn = BotDTMFTurn | BotTextTurn | BotToolTurn;
 
+// represents DTMF tones from the bot
 export interface BotDTMFTurn extends StoreTurn {
   content: string;
   interrupted: boolean;
@@ -23,6 +26,12 @@ export interface BotDTMFTurn extends StoreTurn {
   type: "dtmf";
 }
 
+export type BotDTMFTurnParams = Omit<
+  BotDTMFTurn,
+  "callSid" | "createdAt" | "interrupted" | "id" | "order" | "role" | "type"
+>;
+
+// represents a text from LLM that will be spoken
 export interface BotTextTurn extends StoreTurn {
   content: string;
   interrupted: boolean;
@@ -30,11 +39,25 @@ export interface BotTextTurn extends StoreTurn {
   type: "text";
 }
 
+export type BotTextTurnParams = Omit<
+  BotTextTurn,
+  "callSid" | "createdAt" | "interrupted" | "id" | "order" | "role" | "type"
+>;
+
+// represents the LLM requesting a FN tool be executed
+// note: the results are stored on the toolcall and not a separate item like some LLM APIs, such as OpenAI
 export interface BotToolTurn extends StoreTurn {
   role: "bot";
   tool_calls: ToolCall[];
   type: "tool";
 }
+
+export type BotToolTurnParams = Omit<
+  BotToolTurn,
+  "callSid" | "createdAt" | "role" | "type"
+> & {
+  id?: string;
+};
 
 export interface ToolCall {
   function: { name: string; arguments: any };
@@ -44,14 +67,24 @@ export interface ToolCall {
   type: "function";
 }
 
-// human turns
+/****************************************************
+ Human Turns
+****************************************************/
 export type HumanTurn = HumanDTMFTurn | HumanTextTurn;
 
+// represents DTMF tones from the bot
 export interface HumanDTMFTurn extends StoreTurn {
   content: string;
   role: "human";
   type: "dtmf";
 }
+
+export type HumanDTMFTurnParams = Omit<
+  HumanDTMFTurn,
+  "callSid" | "createdAt" | "id" | "order" | "role" | "type"
+> & {
+  id?: string;
+};
 
 export interface HumanTextTurn extends StoreTurn {
   content: string;
@@ -59,8 +92,22 @@ export interface HumanTextTurn extends StoreTurn {
   type: "text";
 }
 
-// system message
+export type HumanTextTurnParams = Omit<
+  HumanTextTurn,
+  "callSid" | "createdAt" | "id" | "order" | "role" | "type"
+> & {
+  id?: string;
+};
+
+/****************************************************
+ System Turn
+****************************************************/
 export interface SystemTurn extends StoreTurn {
   content: string;
   role: "system";
 }
+
+export type SystemTurnParams = Omit<
+  SystemTurn,
+  "callSid" | "createdAt" | "id" | "order" | "role"
+> & { id?: string };
