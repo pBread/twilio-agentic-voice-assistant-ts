f0bf6c369208239cbb36bbf75ec9da6f1234abd2
pBread <4473570+pBread@users.noreply.github.com>
2025-02-12 10:17:51 -0700

    implemented websocket route

--- completion-server/routes.ts
+++ completion-server/routes.ts
@@ -5,6 +5,8 @@ import { SessionManager } from "./session-manager";
 import { ConversationRelayAdapter } from "./twilio/conversation-relay-adapter";
 import { makeConversationRelayTwiML } from "./twilio/twiml";
 import { TwilioCallWebhookPayload } from "./twilio/voice";
+import { LLMService } from "./llm-service";
+import { ConsciousAgentRuntime } from "../agents/conscious/agent-runtime";
 
 const router = Router();
 
@@ -14,10 +16,26 @@ const router = Router();
 router.post("/incoming-call", async (req, res) => {
   const { CallSid: callSid } = req.body as TwilioCallWebhookPayload;
 
-  const twiml = makeConversationRelayTwiML({ callSid, context: {} });
+  try {
+    const twiml = makeConversationRelayTwiML({ callSid, context: {} });
+    res.status(200).type("text/xml").end(twiml);
+  } catch (error) {
+    log.error("/incoming-call", "unknown error", error);
+    res.status(500).json({ status: "error", error });
+  }
 });
 
-router.post("/call-status", async (req, res) => {});
+router.post("/call-status", async (req, res) => {
+  const callSid = req.body.CallSid;
+  const callStatus = req.body.CallStatus;
+
+  log.info(
+    "/call-status",
+    `call status updated to ${callStatus}, CallSid ${callSid}`
+  );
+
+  res.status(200).send();
+});
 
 /****************************************************
  Outbound Calling Routes
@@ -40,15 +58,72 @@ export const conversationRelayWebsocketHandler: WebsocketRequestHandler = (
   req
 ) => {
   const { callSid } = req.params;
+  log.info("/convo-relay", `websocket initializing, CallSid ${callSid}`);
 
   const relay = new ConversationRelayAdapter(ws);
   const session = new SessionManager(callSid);
 
+  const agent = new ConsciousAgentRuntime(
+    session,
+    { model: "gpt-4" },
+    {
+      instructionTemplate: "You are a friendly robot who likes to tell jokes",
+      tools: [],
+    }
+  );
+
+  const llm = new LLMService(session, agent);
+
   relay.onSetup((ev) => {
     // handle setup
+    const params = ev.customParameters ?? {};
+    const context = "context" in params ? JSON.parse(params.context) : {};
   });
 
-  const turns = session.turns.list();
+  relay.onPrompt((ev) => {
+    if (!ev.last) return; // do nothing on partial speech
+    log.info(`relay.prompt`, `"${ev.voicePrompt}"`);
+
+    session.turns.addHumanText({ content: ev.voicePrompt });
+    llm.doCompletion();
+  });
+
+  relay.onInterrupt((ev) => {
+    log.info(`relay.interrupt`, `human interrupted bot`);
+
+    llm.abort();
+  });
+
+  relay.onDTMF((ev) => {
+    log.info(`relay.dtmf`, `dtmf (human): ${ev.digit}`);
+  });
+
+  // relay.onError only emits errors received from the ConversationRelay websocket, not local errors.
+  relay.onError((ev) => {
+    log.error(`relay.error`, `ConversationRelay error: ${ev.description}`);
+  });
+
+  llm.on("text-chunk", (text, last, fullText) => {
+    relay.sendTextToken(text, last); // send each token as it is received
+
+    if (last && fullText) log.info("llm.transcript", `"${fullText}"`);
+  });
+
+  llm.on("dtmf", (digits) => {
+    relay.sendDTMF(digits);
+    log.info("llm", `dtmf (bot): ${digits}`);
+  });
+
+  ws.on("close", () => {
+    log.info(
+      "relay",
+      "conversation relay ws closed.",
+      "session turns:\n",
+      JSON.stringify(session.turns.list()),
+      "session context:\n",
+      JSON.stringify(session.context)
+    );
+  });
 };
 
 /****************************************************
