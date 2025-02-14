49954092f903f6e5f86900b35e4c6dcd7ece1a50
pBread <4473570+pBread@users.noreply.github.com>
2025-02-14 11:54:33 -0700

    working through types

--- scripts/supabase/setup.ts
+++ scripts/supabase/setup.ts
@@ -1,1 +1,6 @@
 // create user database table
+// add email validation
+// add mobilePhone validation
+
+// (optional) create row-level security
+// enable realtime
--- shared/context.ts
+++ shared/context.ts
@@ -1,7 +1,37 @@
-export interface Context {}
+export interface Context {
+  user?: UserRecord;
+}
 
-export interface UserRecord {
+interface BaseRecord {
   id: string;
+  created_at: Date;
+}
+
+export interface UserRecord extends BaseRecord {
   firstName: string;
   lastName: string;
+
+  email?: string;
+  mobilePhone?: string;
+
+  paymentMethods: PaymentMethod[];
+
+  city?: string;
+  state?: string;
+  zip?: string;
+}
+
+interface PaymentMethod extends BaseRecord {
+  lastFour: string;
+  type: "card";
+}
+
+export interface Order extends BaseRecord {
+  userId: string;
+  id: string;
+  lines: OrderLine[];
+}
+
+interface OrderLine extends BaseRecord {
+  id: string;
 }
