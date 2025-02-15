7ef9b56928838e9f9ad045dab95717248f725527
pBread <4473570+pBread@users.noreply.github.com>
2025-02-14 17:01:29 -0700

    updated context

--- shared/context.ts
+++ shared/context.ts
@@ -1,5 +1,17 @@
-import { UserRecord } from "./db-entities.js";
+import type { UserRecord } from "./db-entities.js";
 
 export interface Context {
+  today: Date;
+
   user?: UserRecord;
+  call: CallDetails;
+}
+
+interface CallDetails {
+  callSid: string;
+  from: string; // e164
+  to: string; // e164
+
+  startTime: string; // 24 hour format, 13:00
+  userPhone: string; // e164
 }
