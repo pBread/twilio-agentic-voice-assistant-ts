ea998f3115e35c1f968b7cf084641bd7372718b0
pBread <4473570+pBread@users.noreply.github.com>
2025-02-20 15:47:34 -0700

    adding tools

--- agent/instructions/200-formatting-for-speech.md
+++ agent/instructions/200-formatting-for-speech.md
@@ -7,18 +7,31 @@ Your responses will be spoken aloud to the user via a text-to-speech service. It
 
 - **Concise and Conversational**: Keep your responses concise, direct, and conversational.
 
-- **Special Data Types**: There are several data types that require specific formatting.
-  - **Numbers & String Identifiers**: Add spacing between characters when reading string identifiers or string numbers.
+## Special Data Types
+
+There are several data types that require specific formatting.
+
+- **Numbers & String Identifiers**:
+  - Add spacing between characters when reading string identifiers or string numbers.
     - Text-to-speech will translate numbers into words, which is coherent only when communicating a true numberical value, like a currency. Numbers that are part of a string must be separated by a space for them to be coherent.
-    - For instance, "Your confirmation number is CN-24-12-01" should be "Your confirmation number is C N - 2 4 - 1 2 - 0 1".
-    - Apply this logic to all special identifiers, not just the ones listed here.
-- **Phone Numbers**: Enunciate each character separately and do not include "+".
-  - Example: "+12223334444" should be "1 2 2 2 3 3 3 4 4 4 4"
-- **Email Addresses**: Enunciate each character separately and replace symbols with words.
+    - For instance, "Your confirmation number is OR-24-12-01" should be "Your confirmation number is O R - 2 4 - 1 2 - 0 1".
+  - Apply this logic to all special identifiers, not just the ones listed here.
+- **Phone Numbers**:
+
+  - Enunciate each character separately and do not include "+".
+    - Example: "+12223334444" should be "1 2 2 2 3 3 3 4 4 4 4"
+  - It is not always necessary to read the entire phone number. If you already have a phone number from their user profile or the call details, you can simply refer to the last 4 characters.
+
+- **Email Addresses**:
+
+  - Enunciate each character separately and replace symbols with words.
   - Example: "jsmith@gmail.com" should be "j s m i t h at gmail dot com"
+
 - **Dates**:
+
   - When speaking, format dates as "Month Day, Year". Example: "April 15, 2025".
   - When calling tools, always use "YEAR-MO-DA". Example: "2025-04-15"
+
 - **Times**:
 
   - When speaking, use 12-hour format with "AM" or "PM". Example: "7:30 PM"
--- agent/tools/functions.ts
+++ agent/tools/functions.ts
@@ -1,5 +1,5 @@
-import type { ToolDefinition, ToolParameters } from "../types.js";
 import { db } from "../../integration-server/mock-database.js";
+import type { ToolDefinition, ToolParameters } from "../types.js";
 
 /****************************************************
  Get User Profile
@@ -42,5 +42,68 @@ export const getUserByEmailOrPhone: ToolDefinition<GetProfile> = {
 
     if (user) deps.store.setContext({ user });
     return user;
+  },
+};
+
+/****************************************************
+ Get Order By Id
+****************************************************/
+const GetOrberByConfirmationNumberParams: ToolParameters = {
+  type: "object",
+  properties: {
+    orderId: { type: "string", description: "The ID of the order" },
+  },
+  required: ["orderId"],
+};
+
+interface GetOrberByConfirmationNumber {
+  orderId: string;
+}
+
+export const getOrderByConfirmationNumber: ToolDefinition<GetOrberByConfirmationNumber> =
+  {
+    name: "getOrderByConfirmationNumber",
+    description: "Find a user by their email address or their phone number.",
+    parameters: GetOrberByConfirmationNumberParams,
+    type: "function",
+    async fn(args: GetOrberByConfirmationNumber, deps) {
+      await new Promise((resolve) => setTimeout(() => resolve(null), 500));
+
+      const orderId = args.orderId.replace(/\-/g, "").toLowerCase();
+
+      return db.orders.find(
+        (order) => order.id.replace(/\-/g, "").toLowerCase() === orderId,
+      );
+    },
+  };
+
+/****************************************************
+ Get User Orders
+****************************************************/
+const GetUserOrdersParams: ToolParameters = {
+  type: "object",
+  properties: {
+    userId: { type: "string", description: "The user id from the user record" },
+  },
+  required: ["userId"],
+};
+
+interface GetUserOrders {
+  userId: string;
+}
+
+export const getUserOrders: ToolDefinition<GetUserOrders> = {
+  name: "getUserOrders",
+  description: "Find a user by their email address or their phone number.",
+  parameters: GetUserOrdersParams,
+  type: "function",
+  async fn(args: GetUserOrders, deps) {
+    await new Promise((resolve) => setTimeout(() => resolve(null), 500));
+
+    const userId = args.userId.replace(/\-/g, "").toLowerCase();
+
+    return db.orders.filter(
+      (order) => order.user_id.replace(/\-/g, "").toLowerCase() === userId,
+    );
   },
 };
