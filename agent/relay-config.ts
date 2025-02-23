0096573f21c19d71351afa04f893d69560ecfc22
pBread <4473570+pBread@users.noreply.github.com>
2025-02-23 09:46:39 -0700

    updated relay config

--- agent/relay-config.ts
+++ agent/relay-config.ts
@@ -1,73 +1,16 @@
 import type { ConversationRelayAttributes } from "twilio/lib/twiml/VoiceResponse.js";
 
-// LtPsVjX1k0Kl4StEMZPK: (Sophia) Good pace and meets criteria
-// nBoLwpO4PAjQaQwVKPI1: () Good pace and meets criteria
-// 9Ft9sm9dzvprPILZmLJl: (Patrick)  Good pace and authoritative
-// HDA9tsk27wYi3uq0fPcK (Stuart) Slightly younger
-// Jessica-flash_v2_5
-// Mark-Natural Conversations UgBBYS2sOqTuMpoF3BR0
-// NHRgOEwqx5WZNClv5sat,Chelsea,HQ,
-// 54Cze5LrTSyLgbO6Fhlc,Cat - Droll and Dry,HQ,
-// 56AoDkrOh6qfVPDXZ7Pt,Cassidy,HQ,
-// SAz9YHcvj6GT2YYXdXww,River,Default,
-// M563YhMmA0S8vEYwkgYa,Sammy Sweet and Quirky,Professional,
-// google: en-US-Journey-D, en-US-Journey-F, en-US-Journey-O, en-IN-Journey-D, en-IN-Journey-F, en-GB-Journey-D, en-GB-Journey-F, de-DE-Journey-D, de-DE-Journey-F
-// Amazon: Amy-Generative, Matthew-Generative, Ruth-Generative
-
-export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-  ttsProvider: "google",
-  voice: "en-US-Journey-D",
+const goodTTS = {
+  google: { journeyD: { ttsProvider: "google", voice: "en-US-Journey-D" } },
+  elevenLabs: {
+    mark: { ttsProvider: "ElevenLabs", voice: "UgBBYS2sOqTuMpoF3BR0" }, // conversational, natural
+    cassidy: { ttsProvider: "ElevenLabs", voice: "56AoDkrOh6qfVPDXZ7Pt" }, //
+    grandpaSpuds: { ttsProvider: "ElevenLabs", voice: "NOpBlnGInO9m6vDvFkFC" }, // humorous, disarming
+    james: { ttsProvider: "ElevenLabs", voice: "EkK5I93UQWFDigLMpZcX" }, // husky, engaging
+    ana: { ttsProvider: "ElevenLabs", voice: "rCmVtv8cYU60uhlsOo1M" }, // soft, british
+  },
 };
 
-// // Mark-Natural Conversations UgBBYS2sOqTuMpoF3BR0
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "UgBBYS2sOqTuMpoF3BR0",
-// };
-
-// // Cassidy 56AoDkrOh6qfVPDXZ7Pt
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "56AoDkrOh6qfVPDXZ7Pt",
-// };
-
-// // Grampa Spuds
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "NOpBlnGInO9m6vDvFkFC",
-// };
-
-// // Russel – Dramatic British (B)
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "NYC9WEgkq1u4jiqBseQ9",
-// };
-
-// // Ana – soft British female voice (B)
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "rCmVtv8cYU60uhlsOo1M",
-// };
-
-// // Finn – fast paced, male voice (C)
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "vBKc2FfBKJfcZNyEt1n6",
-// };
-
-// // John Doe – Deep voice, scary story teller (B)
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "EiNlNiXeDU1pqqOPrYMO",
-// };
-
-// // James - Husky & Engaging EkK5I93UQWFDigLMpZcX
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "EkK5I93UQWFDigLMpZcX",
-// };
-
-// export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
-//   ttsProvider: "ElevenLabs",
-//   voice: "GzE4TcXfh9rYCU9gVgPp",
-// };
+export const relayConfig: Omit<ConversationRelayAttributes, "url"> = {
+  ...goodTTS.elevenLabs.mark,
+};
