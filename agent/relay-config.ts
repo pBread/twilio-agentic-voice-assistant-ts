bf81ca69b9f2912121fd772175c20c7f67b70aa4
pBread <4473570+pBread@users.noreply.github.com>
2025-02-24 17:17:17 -0700

    added more voices

--- agent/relay-config.ts
+++ agent/relay-config.ts
@@ -8,6 +8,8 @@ const goodTTS = {
     grandpaSpuds: { ttsProvider: "ElevenLabs", voice: "NOpBlnGInO9m6vDvFkFC" }, // humorous, disarming
     james: { ttsProvider: "ElevenLabs", voice: "EkK5I93UQWFDigLMpZcX" }, // husky, engaging
     ana: { ttsProvider: "ElevenLabs", voice: "rCmVtv8cYU60uhlsOo1M" }, // soft, british
+    adamStone: { ttsProvider: "ElevenLabs", voice: "NFG5qt843uXKj4pFvR7C" }, // Adam Stone - late night radio
+    theo: { ttsProvider: "ElevenLabs", voice: "NyxenPOqNyllHIzSoPbJ" }, // Theo - Smart, warm, open
   },
 };
 
